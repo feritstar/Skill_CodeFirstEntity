ab95135603c75c90e498dd3b0255a5fb2cc5f363
FeritYILDIZ <feritstar@gmail.com>
2023-06-25 21:56:04 +0300

    Add project files.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirstEntity.Models.MyClass;
+
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            Context context = new Context();
+            var query = context.Skills.ToList();
+            return View(query);
+        }
+    }
+}
---
+++ Models/MyClass/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Skill_CodeFirstEntity.Models.MyClass
+{
+    public class Skill
+    {
+        [Key]
+        public int SkillId { get; set; }
+        public string SkillName { get; set; }
+        public byte PercentageValue { get; set; }
+    }
+}
